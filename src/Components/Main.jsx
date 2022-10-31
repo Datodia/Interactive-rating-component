fe26c28628a65f5e87d1297d5b302a177fce032a
Datodia <dato.diasamidze.02@gmail.com>
2022-10-31 22:00:28 +0400

    feat: build main div markup

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -4,11 +4,16 @@ import styled from 'styled-components'
 export const Main = () => {
 
     const Main = styled.div`
-        width: 440px;
+        width: 412px;
+        height: 416px;
         border: 2px solid red;
+        border-radius: 30px;
+        background: radial-gradient(98.96% 98.96% at 50% 0%, #232A34 0%, #181E27 100%);
         display: flex;
         margin: auto;
+        margin-top: 30vh;
         align-items: center;
+        justify-content: center;
     `
     const Text = styled.h1`
         color: white;
