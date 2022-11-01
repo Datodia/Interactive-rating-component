463ed07beb674bc4084fd3c9e63b9b14829188d4
Datodia <dato.diasamidze.02@gmail.com>
2022-11-01 21:16:56 +0400

    feat: add star component

---
+++ src/Components/Star.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import star from '../assets/star.svg'
+
+export const Star = () => {
+    const Star = styled.div`
+        width: 48px;
+        height: 48px;
+        border-radius: 50%;
+        background-color: #262E38;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 32px 32px;
+    `
+
+    return (
+        <Star>
+            <img src={star} />
+        </Star>
+    )
+}
