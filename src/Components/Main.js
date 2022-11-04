2eba671c94194dcfbcb078dde83ac091ed7da6e7
Datodia <dato.diasamidze.02@gmail.com>
2022-11-04 16:22:48 +0400

    feat: update packeje json

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -29,39 +29,74 @@ export const Main = () => {
         { num && setShow(false) }
     }
 
+    return (
+        <Main>
+            {show ? (
+                <>
+                    <Star />
+                    <Content
+                        head="How did we do?"
+                        para="Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!"
+                    />
+                    <Buttons
+                        setValue={setNum}
+                        value={num}
+                    />
+                    <Submit
+                        click={handleClick}
+                    />
+                </>
+            ) : (
+                <>
+                    <Thanks
+                        value={num}
+                    />
+                    <Content
+                        style={{
+                            textAlign: 'center'
+                        }}
+                        head="Thank You!"
+                        para="We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!"
+                    />
+                </>
+            )}
+        </Main>
+    )
 
-    if (show) {
-        return (
-            <Main>
-                <Star />
-                <Content
-                    head="How did we do?"
-                    para="Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!"
-                />
-                <Buttons
-                    setValue={setNum}
-                />
-                <Submit
-                    click={handleClick}
-                />
-            </Main>
-        )
-    } else {
-        return (
-            <Main>
-                <Thanks
-                    value={num}
-                />
-                <Content
-                    style={{
-                        textAlign: 'center'
-                    }}
-                    head="How did we do?"
-                    para="Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!"
-                />
-            </Main>
-        )
-    }
+
+    // if (show) {
+    //     return (
+    // <Main>
+    //     <Star />
+    //     <Content
+    //         head="How did we do?"
+    //         para="Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!"
+    //     />
+    //     <Buttons
+    //         setValue={setNum}
+    //         value={num}
+    //     />
+    //     <Submit
+
+    //     />
+    // </Main>
+    //     )
+    // } else {
+    //     return (
+    // <Main>
+    //     <Thanks
+    //         value={num}
+    //     />
+    //     <Content
+    //         style={{
+    //             textAlign: 'center'
+    //         }}
+    //         head="Thank You!"
+    //         para="We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!"
+    //     />
+    // </Main>
+    //     )
+    // }
 
 
 }
--- src/Components/Star.js
+++ src/Components/Star.js
@@ -3,7 +3,14 @@ import styled from 'styled-components'
 import star from '../assets/star.svg'
 
 export const Star = () => {
-    const Star = styled.div`
+    return (
+        <Stars>
+            <img src={star} />
+        </Stars>
+    )
+}
+
+const Stars = styled.div`
         width: 48px;
         height: 48px;
         border-radius: 50%;
@@ -21,10 +28,3 @@ export const Star = () => {
             margin-top: 24px;
         }
     `
-
-    return (
-        <Star>
-            <img src={star} />
-        </Star>
-    )
-}
